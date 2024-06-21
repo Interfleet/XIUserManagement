8ff8293d5d9c452d839c4088e8f7b4cbd34229a0
Yogita Bhore <Yogita.Bhore@atkinsglobal.com>
2024-06-21 17:03:08 +0530

    Committing staged changes

--- XIUserManagement/Program.cs
+++ XIUserManagement/Program.cs
@@ -18,7 +18,12 @@ builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddSingleton<AuthenticationService>();
 builder.Services.AddSingleton<UserService>();
 builder.Services.AddSingleton<IAuthorization, Authorization>();
-builder.Services.AddSession();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(4);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 builder.Services.AddMemoryCache();
 
 
